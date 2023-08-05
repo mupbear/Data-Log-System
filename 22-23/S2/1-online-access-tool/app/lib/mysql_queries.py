58c1520dc2ba03ad9cee08fa9c35d608677773cd
mupbear <dana.school.aa@gmail.com>
2023-08-05 16:03:32 +0200

    Added dataloggergui folder, otherwise wont work with docker

--- 22-23/S2/1-online-access-tool/app/lib/mysql_queries.py
+++
@@ -1,2 +0,0 @@
-QUERY_SELECT_FILTERED_RAW_DATA = """SELECT * FROM raw_data WHERE ID > %s AND users_username = %s AND Time >= %s AND Time <= %s AND Base_ID in %s ORDER BY ID ASC LIMIT 1000000;"""
-                                 
